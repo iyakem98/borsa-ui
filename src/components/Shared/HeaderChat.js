d88e7f1a3064d99f5b3e7bbb5a8cf9c465ae607c
Iyakem Estifanos <iyakem16@gmail.com>
2023-05-30 15:47:50 -0500

    avatars

--- src/components/Shared/HeaderChat.js
+++ src/components/Shared/HeaderChat.js
@@ -4,6 +4,37 @@ import { useNavigation } from '@react-navigation/native'
 import { MaterialIcons } from '@expo/vector-icons'
 import { Image } from 'react-native'
 
+const data = [
+  { id: '0', imageSource: require('../../../assets/images/avatars/blank-avatar.png') },
+  { id: '1', imageSource: require('../../../assets/images/avatars/bottts1.png') },
+  { id: '2', imageSource: require('../../../assets/images/avatars/bottts2.png') },
+  { id: '3', imageSource: require('../../../assets/images/avatars/bottts3.png') },
+  { id: '4', imageSource: require('../../../assets/images/avatars/bottts4.png') },
+  { id: '5', imageSource: require('../../../assets/images/avatars/bottts5.png') },
+  { id: '6', imageSource: require('../../../assets/images/avatars/bottts6.png') },
+  { id: '7', imageSource: require('../../../assets/images/avatars/bottts7.png') },
+  { id: '8', imageSource: require('../../../assets/images/avatars/bottts8.png') },
+  { id: '9', imageSource: require('../../../assets/images/avatars/bottts9.png') },
+  { id: '10', imageSource: require('../../../assets/images/avatars/bottts10.png') },
+  { id: '11', imageSource: require('../../../assets/images/avatars/bottts11.png') },
+  { id: '12', imageSource: require('../../../assets/images/avatars/bottts12.png') },
+  { id: '13', imageSource: require('../../../assets/images/avatars/bottts13.png') },
+  { id: '14', imageSource: require('../../../assets/images/avatars/bottts14.png') },
+  { id: '15', imageSource: require('../../../assets/images/avatars/bottts15.png') },
+  { id: '16', imageSource: require('../../../assets/images/avatars/bottts16.png') },
+  { id: '17', imageSource: require('../../../assets/images/avatars/bottts17.png') },
+  { id: '18', imageSource: require('../../../assets/images/avatars/bottts18.png') },
+  { id: '19', imageSource: require('../../../assets/images/avatars/bottts19.png') },
+  { id: '20', imageSource: require('../../../assets/images/avatars/bottts20.png') },
+  // Add more images as needed
+];
+
+const getImageSourceById = (id) => {
+    const item = data.find((item) => item.id === id);
+    return item ? item.imageSource : null;
+  };
+  
+
 const HeaderChat = ({
     isActive,
     user,
@@ -48,7 +79,7 @@ const HeaderChat = ({
                     theUser: userSelectedFromConnectCard
                 })}>
             <Image
-              source={{uri: userSelectedFromConnectCard?.profilePic}}
+              source={getImageSourceById(userSelectedFromConnectCard?.profilePic)}
               style={{
                 height: 50,
                 width: 50,
