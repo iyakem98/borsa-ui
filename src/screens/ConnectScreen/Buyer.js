184c484ed3eb5f56bd73df39448aff5444fe9369
Iyakem Estifanos <iyakem16@gmail.com>
2023-05-30 14:38:01 -0500

    lb

--- src/screens/ConnectScreen/Buyer.js
+++ src/screens/ConnectScreen/Buyer.js
@@ -249,7 +249,7 @@ const BuyerChat = async(buyerData)=> {
                             <Text style={{
                                 fontFamily: "Poppins_400Regular",
                                 fontSize: 13
-                            }}>Kg</Text>
+                            }}>kg</Text>
                         </Text>
                     )}
                    
