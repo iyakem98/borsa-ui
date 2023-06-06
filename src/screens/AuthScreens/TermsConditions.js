dc9f7250ff2a4d28ae9e08d9ec4e91621f745a20
Iyakem Estifanos <iyakem16@gmail.com>
2023-06-05 21:06:58 -0500

    termsConditions

--- src/screens/AuthScreens/TermsConditions.js
+++ src/screens/AuthScreens/TermsConditions.js
@@ -24,19 +24,23 @@ const TermsConditions = () => {
       description: '4.1. Borsa acts solely as a communication platform and does not assume liability for any transactions, agreements, or disputes that may arise between users.\n\n4.2. Borsa does not guarantee the accuracy, completeness, or reliability of any information exchanged between users or contained within the App.\n\n4.3. Users acknowledge that Borsa is not responsible for verifying the identity, qualifications, or authenticity of any user, and they agree to exercise caution and conduct their own due diligence before engaging in any transaction.\n\n4.4. Borsa disclaims all warranties, whether express or implied, including but not limited to fitness for a particular purpose, non-infringement, and compatibility with users\' devices.',
     },
     {
-      term: '5.. Intellectual Property',
+      term: '5. Intellectual Property',
       description: '5.1. All content, including but not limited to text, images, logos, and trademarks, contained within the App are the property of Borsa or its licensors and are protected by intellectual property laws.\n\n5.2. Users may not reproduce, modify, distribute, or create derivative works of any content without prior written consent from Borsa.',
     },
     {
-      term: '6. Termination',
-      description: '6.1. Borsa reserves the right to suspend, terminate, or restrict access to the App at any time and for any reason, without prior notice.\n\n6.2. Users may terminate their use of Borsa at any time by uninstalling the App and discontinuing its use.',
+        term: '6. Pre-release Version and Bugs',
+        description: '6.1. Borsa is currently in pre-release version, and as such, may contain bugs, errors, or other technical issues that could impact its functionality or user experience.\n\n6.2. Users acknowledge that the pre-release version of Borsa is provided on an "as is" basis, without warranties of any kind, and Borsa makes no representations or warranties regarding its performance, reliability, or suitability for any purpose.\n\n6.3. Users understand and accept that the pre-release version of Borsa may undergo frequent updates, modifications, or improvements, and some features may be temporarily unavailable or may not work as intended.\n\n6.4. Borsa shall not be held liable for any damages, losses, or inconvenience caused by the use of the pre-release version, including but not limited to data loss, system crashes, or any adverse effects on user devices.\n\n6.5. Users are encouraged to provide feedback, bug reports, or suggestions for improvement to Borsa, which will be greatly appreciated in order to enhance the stability and functionality of the app. ',
+      },
+    {
+      term: '7. Termination',
+      description: '7.1. Borsa reserves the right to suspend, terminate, or restrict access to the App at any time and for any reason, without prior notice.\n\n7.2. Users may terminate their use of Borsa at any time by uninstalling the App and discontinuing its use.',
     },
     {
-      term: '7. Governing Law and Jurisdiction',
+      term: '8. Governing Law and Jurisdiction',
       description: '7.1. This Agreement shall be governed by and construed in accordance with the laws of [Jurisdiction]. Any disputes arising from this Agreement shall be subject to the exclusive jurisdiction of the courts of [Jurisdiction].',
     },
     {
-      term: '8. Miscellaneous',
+      term: '9. Miscellaneous',
       description: '8.1. This Agreement constitutes the entire agreement between the user and Borsa and supersedes any prior understandings or agreements, whether oral or written.\n\n8.2. If any provision of this Agreement is deemed invalid or unenforceable, the remaining provisions shall remain in full force and effect.',
     },
   ];
